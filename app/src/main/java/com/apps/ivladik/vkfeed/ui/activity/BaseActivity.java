a646bd2b222a1a651cb8561bba998a572aefc57b
ivladik <ivladik94@gmail.com>
2017-12-29 20:16:41 +0300

    NewFeedFragment created

--- app/src/main/java/com/apps/ivladik/vkfeed/ui/activity/BaseActivity.java
+++ app/src/main/java/com/apps/ivladik/vkfeed/ui/activity/BaseActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.Toolbar;
 import android.widget.FrameLayout;
 
 import com.apps.ivladik.vkfeed.R;
+import com.apps.ivladik.vkfeed.common.manager.MyFragmentManager;
 import com.apps.ivladik.vkfeed.ui.fragment.BaseFragment;
 import com.arellomobile.mvp.MvpAppCompatActivity;
 
@@ -14,11 +15,15 @@ import com.arellomobile.mvp.MvpAppCompatActivity;
  */
 
 public abstract class BaseActivity extends MvpAppCompatActivity {
+    MyFragmentManager myFragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_base);
+
+        myFragmentManager = new MyFragmentManager();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -31,6 +36,33 @@ public abstract class BaseActivity extends MvpAppCompatActivity {
     protected abstract int getMainContentLayout();
 
     public void fragmentOnScreen(BaseFragment fragment) {
+        setToolbarTitle(fragment.createToolbarTitle(this));
+    }
 
+    public void setToolbarTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
+    public void setContent(BaseFragment fragment) {
+        myFragmentManager.setFragment(this, fragment, R.id.main_wrapper);
+    }
+
+    public void addContent(BaseFragment fragment) {
+        myFragmentManager.addFragment(this, fragment, R.id.main_wrapper);
+    }
+
+    public boolean removeCurrentFragment() {
+        return myFragmentManager.removeCurrentFragment(this);
+    }
+
+    public boolean removeFragment(BaseFragment fragment) {
+        return myFragmentManager.removeFragment(this, fragment);
+    }
+
+    @Override
+    public void onBackPressed() {
+        removeCurrentFragment();
     }
 }
---
+++ app/src/main/java/com/apps/ivladik/vkfeed/ui/fragment/NewsFeedFragment.java
@@ -0,0 +1,33 @@
+package com.apps.ivladik.vkfeed.ui.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.apps.ivladik.vkfeed.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NewsFeedFragment extends BaseFragment {
+
+
+    public NewsFeedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    protected int getMainContentLayout() {
+        return R.layout.fragment_feed;
+    }
+
+    @Override
+    public int onCreateToolbarTitle() {
+        return 0;
+    }
+
+}
